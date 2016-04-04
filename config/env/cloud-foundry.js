d24b9f2825b31bdfc30cfd7cf60e21b4de63c2db
George Torres <gtorre18@fau.edu>
2016-04-04 09:47:17 -0400

    changes to callback url

--- config/env/cloud-foundry.js
+++ config/env/cloud-foundry.js
@@ -35,7 +35,8 @@ module.exports = {
   facebook: {
     clientID: getCred('mean-facebook', 'id') || '1674990372753086',
     clientSecret: getCred('mean-facebook', 'secret') || '50e0af78b3c3adb5ba60c66dce2ffe1d',
-    callbackURL: 'http://smartbullet1.mybluemix.net'
+    callbackURL: 'http://smartbullet1.mybluemix.net/api/auth/facebook/callback'
+    callbackURL: 'http://smartbullet1.mybluemix.net
   },
   twitter: {
     clientID: getCred('mean-twitter', 'key') || '8TMzdUEI1bnOdXAQchBC8VkJx',
